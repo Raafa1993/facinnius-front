b6fa01865fdb115cfbcd2e5f1706f47ee8c1e5c8
Raafa1993 <rafaelajds@gmail.com>
2022-10-31 23:49:41 -0300

    update files

--- src/pages/products/[slug].tsx
+++ src/pages/products/[slug].tsx
@@ -1,20 +1,25 @@
+import { t } from "i18next";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
-import { ProdutosData } from "../../data/ProdutosData";
+import { ProdutosBrData } from "../../data/ProdutosBrData";
+import { ProdutosEnData } from "../../data/ProdutosEnData";
 
 export default function Products(props: any) {
   const router = useRouter();
+  const { t, i18n } = useTranslation();
 
+  const isBr = i18n.language === 'ptbr' ? ProdutosBrData : ProdutosEnData
   const [filterProductsId, setFilterProductsId] = useState<any>({});
 
   useEffect(() => {
     setFilterProductsId({
-      ...ProdutosData.filter((obj) => obj.id === Number(router.query.id))[0],
+      ...isBr.filter((obj) => obj.id === Number(router.query.id))[0],
     });
-  }, []);
+  }, [isBr]);
 
   return (
     <>
@@ -23,8 +28,7 @@ export default function Products(props: any) {
         <section className="quality section" id="premium">
           <div className="quality__container container">
             <h2 className="section__title">
-              Oferecemos uma preparação premium e de melhor qualidade só para
-              você!
+              {t('produtoId_titulo')}
             </h2>
 
             <div className="quality__content grid">
